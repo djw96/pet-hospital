070b047d80a86fc3131b614818513d2e2ec4ada3
djw <jellybeanmx@gmail.com>
2018-04-11 18:25:36 +0800

    add vuex

--- pet/src/main.js
+++ pet/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
@@ -8,6 +9,7 @@ import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+Vue.use(Vuex)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
