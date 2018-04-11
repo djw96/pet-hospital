903fdb8cc001bae50472ea1afec4f5b0e8b0a8d8
djw <jellybeanmx@gmail.com>
2018-04-11 17:48:39 +0800

    import element ui

---
+++ pet/src/components/Ele.vue
@@ -0,0 +1,28 @@
+<template>
+    <el-table :data="tableData">
+        <el-table-column prop="id" label="id"></el-table-column>
+        <el-table-column prop="name" label="name"></el-table-column>
+    </el-table>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          id: 1,
+          name: 'djw'
+        },
+        {
+          id: 2,
+          name: 'djw2'
+        },
+        {
+          id: 3,
+          name: 'djw3'
+        }
+      ]
+    }
+  }
+}
+</script>
--- pet/src/router/index.js
+++ pet/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import SignIn from '@/components/SignIn'
 import Index from '@/components/Index'
+import Ele from '@/components/Ele'
 Vue.use(Router)
 
 export default new Router({
@@ -13,6 +14,10 @@ export default new Router({
     {
       path: '/index',
       component: Index
+    },
+    {
+      path: '/ele',
+      component: Ele
     }
   ]
 })
