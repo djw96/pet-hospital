f29e1eeba61e239aa43723015eb6e7b3305af274
djw <jellybeanmx@gmail.com>
2018-04-19 15:56:55 +0800

    修改3D导览，自定义组件

--- pet/src/components/Ele.vue
+++ pet/src/components/Ele.vue
@@ -1,28 +1,36 @@
 <template>
   <div>
-    <h1>{{$route.params.id}}</h1>
-    <el-table :data="tableData">
-      <el-table-column prop="id" label="id"></el-table-column>
-      <el-table-column prop="name" label="name"></el-table-column>
-    </el-table>
+    <p>{{message}}</p>
+    <time-down @time-end="message = '计时结束'" :endTime="endTime"></time-down>
+    <question :text="text" :options="options"></question>
   </div>
 </template>
 <script>
+import TimeDown from './TimeDown'
+import Question from './Question'
 export default {
+  components: {
+    'time-down': TimeDown,
+    'question':Question
+  },
   data() {
     return {
-      tableData: [
+      message: '正在计时',
+      endTime: '2018-04-19 14:16:00',
+      text:'试题',
+      options:[
         {
-          id: 1,
-          name: 'djw1'
-        },
-        {
-          id: 2,
-          name: 'djw2'
-        },
-        {
-          id: 3,
-          name: 'djw3'
+          name:'A',
+          text:'选项A'
+        },{
+          name:'B',
+          text:'选项B'
+        },{
+          name:'C',
+          text:'选项C'
+        },{
+          name:'D',
+          text:'选项D'
         }
       ]
     }
